b58f198683f27e7d46e6a229b5b6ca3d058cf7de
Raul <168528192+Raulisr00t@users.noreply.github.com>
2025-07-06 11:39:29 +0400

    Create EntryPoint.c

---
+++ EntryPoint.c
@@ -0,0 +1,10 @@
+void entry(void)
+
+{
+  CheckTimeTIDPID();
+                    /* This Function checks System Time, TickCount and some PID, TID staff!
+                       
+                       Initilization() function parses some commandline arguments and execution! */
+  Initilization();
+  return;
+}
