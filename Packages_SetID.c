ae1c7b257400c2eab05ede158d426b67e3b9ff48
Raul <168528192+Raulisr00t@users.noreply.github.com>
2025-07-21 20:33:41 +0400

    Create Packages_SetID.c

---
+++ Packages_SetID.c
@@ -0,0 +1,32 @@
+
+void Packages_SetID(OLECHAR *param_1)
+
+{
+  longlong lVar1;
+  longlong *plVar2;
+  BSTR pOVar3;
+  undefined8 uVar4;
+  longlong local_res10 [3];
+  
+  local_res10[0] = 0;
+  plVar2 = TroubleShootingPackages(local_res10);
+  lVar1 = local_res10[0];
+  if ((int)plVar2 < 0) {
+    uVar4 = 0x948;
+  }
+  else {
+    if (*(BSTR *)(local_res10[0] + 0x20) != (BSTR)0x0) {
+      SysFreeString(*(BSTR *)(local_res10[0] + 0x20));
+      *(undefined8 *)(lVar1 + 0x20) = 0;
+    }
+    pOVar3 = SysAllocString(param_1);
+    *(BSTR *)(lVar1 + 0x20) = pOVar3;
+    if (param_1 != (OLECHAR *)0x0) {
+      return;
+    }
+    uVar4 = 0x94d;
+  }
+  PrintERROR(1,"Dwz ERROR: %s:%d - hr = 0x%08X\n","Packages_SetID",uVar4);
+  return;
+}
+
