461e9e11748618740ac7d3a631b75c2e6e3e8590
Raul <168528192+Raulisr00t@users.noreply.github.com>
2025-07-25 22:26:20 +0400

    Create Setkeywords.c

---
+++ Setkeywords.c
@@ -0,0 +1,28 @@
+
+void Setkeywords(OLECHAR *param_1)
+
+{
+  longlong lVar1;
+  longlong *plVar2;
+  BSTR pOVar3;
+  longlong local_res10 [3];
+  
+  local_res10[0] = 0;
+  plVar2 = TroubleShootingPackages(local_res10);
+  lVar1 = local_res10[0];
+  if ((int)plVar2 < 0) {
+    PrintERROR(1,"Dwz ERROR: %s:%d - hr = 0x%08X\n","Packages_SetKeywords",0x9bd);
+  }
+  else {
+    if (*(BSTR *)(local_res10[0] + 0x58) != (BSTR)0x0) {
+      SysFreeString(*(BSTR *)(local_res10[0] + 0x58));
+      *(undefined8 *)(lVar1 + 0x58) = 0;
+    }
+    if (param_1 != (OLECHAR *)0x0) {
+      pOVar3 = SysAllocString(param_1);
+      *(BSTR *)(lVar1 + 0x58) = pOVar3;
+    }
+  }
+  return;
+}
+
