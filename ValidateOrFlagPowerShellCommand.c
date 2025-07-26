9cc4864b61c215dbc73d63180002ef666830f497
Raul <168528192+Raulisr00t@users.noreply.github.com>
2025-07-26 12:31:15 +0400

    Update and rename InterestingFunction.c to ValidateOrFlagPowerShellCommand.c

--- ValidateOrFlagPowerShellCommand.c
+++ ValidateOrFlagPowerShellCommand.c
@@ -1,5 +1,4 @@
-
-undefined8 InterestingFunction(wchar_t *any_string,undefined8 param_2,undefined8 param_3)
+undefined8 ValidateOrFlagPowerShellCommand(wchar_t *input,undefined8 param_2,undefined8 context)
 
 {
   char cVar1;
@@ -7,21 +6,21 @@ undefined8 InterestingFunction(wchar_t *any_string,undefined8 param_2,undefined8
   undefined4 *puVar3;
   undefined *puVar4;
   
-  if (any_string != (wchar_t *)0x0) {
+  if (input != (wchar_t *)0x0) {
     puVar4 = &DAT_14006d95c;
-    pwVar2 = wcsstr(any_string,L"$(");
+    pwVar2 = wcsstr(input,L"$(");
     if (pwVar2 == (wchar_t *)0x0) {
                     /* WARNING: Load size is inaccurate */
       if (*(int *)(*ThreadLocalStoragePointer + 4) < DAT_1400801e8) {
         puVar3 = &DAT_1400801e8;
-        FUN_1400037f4(&DAT_1400801e8);
+        WaitAndLockSharedResource(&DAT_1400801e8);
         if (DAT_1400801e8 == -1) {
-          ISawPowershellCommandExpression(puVar3,puVar4,param_3);
+          FlagSuspiciousPowerShellPatterns(puVar3,puVar4,context);
           FUN_1400034c0(FUN_140062c10);
           _Init_thread_footer(&DAT_1400801e8);
         }
       }
-      cVar1 = FUN_14001ede0(any_string);
+      cVar1 = FUN_14001ede0(input);
       if (cVar1 != '\0') {
         return 0x80070057;
       }
@@ -30,4 +29,3 @@ undefined8 InterestingFunction(wchar_t *any_string,undefined8 param_2,undefined8
   }
   return 0x80070057;
 }
-
