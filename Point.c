e8e55cea7a760184352e580cc4f253e428207a50
Raul <168528192+Raulisr00t@users.noreply.github.com>
2025-08-09 14:15:42 +0400

    Create Point.c

---
+++ Point.c
@@ -0,0 +1,12 @@
+
+void Point(undefined8 param_1)
+
+{
+  if (DAT_14007f5b8 == '\0') {
+    DAT_14007f5b8 = '\x01';
+    PointerEncoder(FUN_140002530);
+  }
+  DAT_14007f578 = param_1;
+  return;
+}
+
